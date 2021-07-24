3c8530156d97abfbff33187bb05b638d7b5e4d80
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-07-24 12:04:16 -0300

    feat: add in memory class repository

---
+++ src/infra/memory/memoryClassRepository.ts
@@ -0,0 +1,21 @@
+import { ClassRepository } from '@/application/repositories/classRepository'
+import { IBaseId, IClass } from '@/domain'
+
+const classes: (IBaseId & IClass)[] = []
+
+export class MemoryClassReporitory implements ClassRepository {
+  async create(classData: IClass): Promise<string> {
+    const newId = `${Date.now()}`
+    classes.push({
+      ...classData,
+      id: newId,
+    })
+    return newId
+  }
+
+  async getById(id: string): Promise<IBaseId & IClass> {
+    const foundClass = classes.find((c) => c.id === id)
+    if (!foundClass) throw new Error(`Not found class by id ${id}`)
+    return foundClass
+  }
+}
