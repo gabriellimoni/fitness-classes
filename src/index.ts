050a48005e13954df7d576a184911d45ec270a4e
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-07-24 11:15:21 -0300

    chore: configure development environment

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+if (process.env.PARSE_MODULES) require('module-alias')('.')
+
+import { a } from '@/placeholder'
+
+console.log(a)
---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+  test('placeholder', () => {
+    expect(true).toBeTruthy()
+  })
+})
